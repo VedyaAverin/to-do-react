40a5b9f2c1738d234833c6c06d256c29a3bd0543
Vlad Averin <averin.project.it@gmail.com>
2021-12-14 20:01:37 +0300

    init: Material UI v4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
+import { Container, makeStyles, Typography } from '@material-ui/core';
 
 function App() {
   const [todos, setTodos] = React.useState([
     { id: 1, completed: false, title: 'Купить Хлеб' },
     { id: 2, completed: false, title: 'Купить Рыбу' },
-    { id: 3, completed: false, title: 'Купить Яйца' }
+    { id: 3, completed: false, title: 'Купить Яйца' },
+    { id: 4, completed: false, title: 'Купить Мясо' },
+    { id: 5, completed: false, title: 'Купить Рыбу' },
+    { id: 6, completed: false, title: 'Купить Яйца' }
   ])
 
   function toggleTodo(id) {
@@ -22,13 +26,26 @@ function App() {
     setTodos(todos.filter(todo => todo.id !== id))
   }
 
+  const useStyles = makeStyles({
+    cont: {
+      margin: 0 + ' auto',
+      backgroundColor: "#bdbdbd",
+      textAlign: 'center',
+      borderRadius: "10px",
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    }
+  })
+
+  const classesCont = useStyles();
+
   return (
     <Context.Provider value={{removeTodo}}>
-      <div className='wrapper'>
-        <h1>React Tutorial</h1>
-
-        <TodoList todos={todos} onToggle={toggleTodo}></TodoList>
-      </div>
+      <Container className={classesCont.cont} fixed>
+        <Typography variant='h3'>To Do List</Typography>
+        <TodoList className='da' todos={todos} onToggle={toggleTodo} />
+      </Container>
     </Context.Provider>
   );
 }
--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -2,17 +2,11 @@ import React from "react";
 import PropTypes from "prop-types"
 import TodoItem from "./TodoItem";
 
-const styles = {
-    ul: {
-        listStyle: 'none',
-        margin: 0,
-        padding: 0
-    }
-}
+
 
 function TodoList(props) {
     return (
-        <ul style={styles.ul}>
+        <ul>
            { props.todos.map((todo, index) => {
             return (
                 <TodoItem 
